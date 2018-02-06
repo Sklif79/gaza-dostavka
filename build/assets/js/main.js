830246f2714fad3c6c5f57323bdbbec60e0a6c00
Sklif79 <sklif_@mail.ru>
2018-02-06 18:01:38 +0300

    все настроил, начал главную

---
+++ build/assets/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+ready(function () {});
+
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
