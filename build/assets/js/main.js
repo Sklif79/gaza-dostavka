017c30a9c64538adcb9de5675e251020d887afaf
Sklif79 <sklif_@mail.ru>
2018-02-07 20:07:02 +0300

    меню главной 07.02.2018

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
-ready(function () {});
+ready(function () {
+    hoverImages('.aside-nav');
+});
 
 function ready(fn) {
     if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
@@ -8,4 +10,25 @@ function ready(fn) {
     } else {
         document.addEventListener('DOMContentLoaded', fn);
     }
+}
+
+function hoverImages(targetHover) {
+    var targetElement = document.documentElement.querySelectorAll(targetHover);
+
+    Array.prototype.targetElement.forEach(function (item) {
+        console.log(item);
+    });
+
+    // let newSrc, src, oldSrc;
+    //
+    // //делегируем события для контейнера
+    // $('.aside-nav__link').on('mouseenter', img, function () {
+    //     newSrc = $(this).attr("data-src-hover");
+    //     oldSrc = $(this).attr("src");
+    //     src = $(this).attr("src", newSrc);
+    // });
+    //
+    // $('.aside-nav__link').on('mouseleave', img, function () {
+    //     $(this).attr("src", oldSrc);
+    // });
 }
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,5 @@
 ready(function(){
-	
+    hoverImages('.aside-nav');
 });
 
 function ready(fn) {
@@ -8,4 +8,33 @@ function ready(fn) {
     } else {
         document.addEventListener('DOMContentLoaded', fn);
     }
+}
+
+
+
+function hoverImages(targetHover) {
+    let targetElement = document.documentElement.querySelectorAll(targetHover);
+
+    Array.prototype.targetElement.forEach(function (item) {
+        console.log(item);
+    });
+
+
+
+
+
+
+
+    // let newSrc, src, oldSrc;
+    //
+    // //делегируем события для контейнера
+    // $('.aside-nav__link').on('mouseenter', img, function () {
+    //     newSrc = $(this).attr("data-src-hover");
+    //     oldSrc = $(this).attr("src");
+    //     src = $(this).attr("src", newSrc);
+    // });
+    //
+    // $('.aside-nav__link').on('mouseleave', img, function () {
+    //     $(this).attr("src", oldSrc);
+    // });
 }
